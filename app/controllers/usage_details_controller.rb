f11ad92e6e311198687a6864a4e4df5859a7e6fe
ttngm <shinagawa003@gmail.com>
2022-03-13 22:06:08 +0900

    fix

--- app/controllers/usage_details_controller.rb
+++ app/controllers/usage_details_controller.rb
@@ -6,12 +6,4 @@ class UsageDetailsController < ApplicationController
 
     @usageAmounts = @usageDetail.usageAmounts;
   end
-
-  def show
-    
-    @usageDetail = UsageDetail.find_by(year: params[:year], month: params[:month]);
-    logger.debug('method:' + @usageDetail.usageAmounts[2].method.to_s)
-
-    # @usageAmounts = @usageDetail.usageAmounts;
-  end
 end
